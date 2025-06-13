5cf7f3258c78f4311c834ee5b902b302ddbab49d
dew <foaming21@gmail.com>
2025-06-13 17:14:16 +0900

    연산문 문제 풀이

---
+++ main/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String text = "long";
+        int n = sc.nextInt();
+
+        for (int i=0; i<n; i++){
+        System.out.print(text + " ");
+        }
+        System.out.print("int");
+    }
+}
